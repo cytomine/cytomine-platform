cytomine/cytomine-platform

commit 32d668a98d888e693fae0b319d8e1048bba0737b

Update dependency tifffile

plugins/pims-plugin-format-openslide/pims_plugin_format_openslide/ndpi.py
@@ -43,7 +43,7 @@ class NDPIParser(TifffileParser):
     def _parsed_ndpi_tags(self) -> dict:
         tags = self.baseline.ndpi_tags
 
-        comments = tags.get("Comments", None)
+        comments = tags.valueof("Comments", None)
         if comments:
             # Comments tag (65449): ASCII key=value pairs (not always present)
             lines = comments.split('\n')

commit 1d20024e6670773b3920fccfc11ffc2ed1aea9c5

Fix NDPI tag parsing issue introduced by 32d668a98d888e693fae0b319d8e1048bba0737b

plugins/pims-plugin-format-openslide/pims_plugin_format_openslide/ndpi.py
@@ -43,7 +43,7 @@ class NDPIParser(TifffileParser):
     def _parsed_ndpi_tags(self) -> dict:
         tags = self.baseline.ndpi_tags
 
-        comments = tags.valueof("Comments", None)
+        comments = tags.get("Comments", None)
         if comments:
             # Comments tag (65449): ASCII key=value pairs (not always present)
             lines = comments.split('\n')