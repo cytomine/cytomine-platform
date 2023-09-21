03fd676e91e932ef1dd367768d3d2a1f4e53e8a3
Romain Mormont <romain.mormont@cytomine.com>
2023-09-21 15:09:41 +0200

    feat: wip tar.gz backup generation

--- files/cytomine_mongo_backup.sh
+++ files/cytomine_mongo_backup.sh
@@ -2,31 +2,33 @@
 
 # PostgreSQL database connection parameters
 DB_HOST="localhost"  # Database host
-DB_PORT="$MONGODB_PORT"  # Database port (default is 27017)
-DB_NAME="$MONGODB_DB_NAME"  # Database name
-DB_USER="$MONGODB_USER"  # Database username
-DB_PASS="$MONGODB_PASS"	 # Database password
+DB_PORT="$MONGO_DB_PORT"  # Database port (default is 27017)
+DB_NAME="$MONGO_INIT_DATABASE"  # Database name
+DB_USER="$MONGO_INITDB_ROOT_USERNAME"  # Database username
+DB_PASS="$MONGO_INITDB_ROOT_PASSWORD"	 # Database password
 
 # Backup directory and filename
 BACKUP_DIR="/backups" 
 BACKUP_FILENAME="cytomine_mongo_backup_$(date "+%A")" # Use day of the week for unique backup directories
 
-mkdir -p $BACKUP_DIR
-
 # Full path to the backup file
 BACKUP_TARGET_PATH="$BACKUP_DIR/$BACKUP_FILENAME.tar.gz"
-BACKUP_TMP_PATH="/tmp/$BACKUP_FILENAME.sql"
+mkdir -p $BACKUP_DIR
+
+# Create and clean temporary directory
+BACKUP_TMP_DIR="/tmp/backups"
+mkdir -p $BACKUP_TMP_DIR
+rm $BACKUP_TMP_DIR/* -r
 
 #logging operation
 echo "Backing up cytomine mongo database : $DB_NAME"
 
 # Create the backup
-export LC_TIME=en_US.UTF-8 # to change the language of the date to English
-mongodump --host "$DB_HOST" --port "$DB_PORT" --username "$DB_USER" --password "$DB_PASS" --out "$BACKUP_DIR"
-tar -czvf $BACKUP_TARGET_PATH $BACKUP_TMP_PATH
+mongodump --host "$DB_HOST" --port "$DB_PORT" --username "$DB_USER" --password "$DB_PASS" --out "$BACKUP_TMP_DIR"
+tar -czvf $BACKUP_TARGET_PATH -C $BACKUP_TMP_DIR .
 
-# Clean tmp file
-rm $BACKUP_TMP_PATH
+# Clean tmp files
+rm $BACKUP_TMP_DIR -r
 
 # Check the exit status of mongodump
 if [ $? -eq 0 ]; then
