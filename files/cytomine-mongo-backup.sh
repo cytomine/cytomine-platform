4fe40c09c9292a9b90942edb1673f2f7b25d727d
Jérôme Gillard <jerome.gillard@cytomine.com>
2023-10-03 09:28:54 +0200

    wip: failed to connect to db.

--- files/cytomine-mongo-backup.sh
+++ files/cytomine-mongo-backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "\n$(data) Start of the backup script."
+echo "\n$(date) Start of the backup script."
 
 # Loading mongo related environment variables
 MONGO_ENV_FILE="/tmp/cytomine.mongo.env"
@@ -20,41 +20,44 @@ DB_PASS="$MONGO_INITDB_ROOT_PASSWORD"	 # Database password
 
 # Backup directory and filename
 BACKUP_DIR="/data/db/backup" 
-BACKUP_FILENAME="cytomine_mongo_backup_$(date "+%A")" # Use day of the week for unique backup directories
+BACKUP_FILENAME="cytomine_mongo_backup_$(date "+%a")" # Use day of the week for unique backup directories
 
 # Full path to the backup file
 BACKUP_TARGET_PATH="$BACKUP_DIR/$BACKUP_FILENAME.tar.gz"
 mkdir -p $BACKUP_DIR
 
-# Create and clean temporary directory
 BACKUP_TMP_DIR="/tmp/backups"
+echo "$(date)  Create and clean temporary directory ${BACKUP_TMP_DIR}"
 mkdir -p $BACKUP_TMP_DIR
-rm $BACKUP_TMP_DIR/* -rf
+rm -rf "${BACKUP_TMP_DIR:?}/*"
 
 #logging operation
-echo "$(date) Backing up cytomine mongo database : $DB_NAME"
+echo "$(date) Backing up cytomine mongo database : $DB_USER@$DB_HOST:$DB_PORT/$DB_NAME into $BACKUP_TMP_DIR "
 
 # Create the backup
+# mongodump --host localhost --port 27017 --username=mongoadmin --password=password --db=cytomine --authenticationDatabase cytomine --out="/tmp/backups/"
+# -> Auth failure
 mongodump --host "$DB_HOST" --port "$DB_PORT" --username "$DB_USER" --password "$DB_PASS" --out "$BACKUP_TMP_DIR"
 # Check the exit status of mongodump
 if [ $? -ne 0 ]; then
   echo -e "$(date) Could not extract Mongo dump. Aborting backup."
-  exit 1
+  exit 2
 fi
 
-cd $BACKUP_TMP_PATH
-tar -czvf $BACKUP_TARGET_PATH -C $BACKUP_TMP_DIR . 2>&1
+cd $BACKUP_TMP_DIR || exit 3
+tar -czf "$BACKUP_TARGET_PATH" ./* 2>&1
 # Check the exit status of tar
 if [ $? -eq 0 ]; then
   echo "$(date) Backup completed successfully: $BACKUP_TARGET_PATH"
 else
   echo "$(date) Backup failed"
   # Clean tmp file
-  rm "$BACKUP_TMP_DIR/*" -rf
-  exit 2
+  rm -rf "${BACKUP_TMP_DIR:?}/*"
+  exit 4
 fi
 
 # Clean tmp files
 rm $BACKUP_TMP_DIR -rf
 
 echo "$(date) Backup completed successfully: $BACKUP_DIR"
+exit 0
--- files/start-crond.sh
+++ files/start-crond.sh
@@ -2,7 +2,8 @@
 
 # The file /mongo.env is needed by the backup script 
 # to retrieve mongo db credentials to run the backups.
-printenv | grep MONGO >> /tmp/cytomine.mongo.env
+mkdir -p /tmp
+printenv | grep MONGO > /tmp/cytomine.mongo.env
 
 service cron start
 
