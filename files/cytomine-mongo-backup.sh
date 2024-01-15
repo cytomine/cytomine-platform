d6497243e178dcd5de6ccd6967bb7f95cc295bd4
Juliette Ponnet <juliette.ponnet@cytomine.com>
2024-01-15 16:50:12 +0100

    feat: add monthly backups

--- files/cytomine-mongo-backup.sh
+++ files/cytomine-mongo-backup.sh
@@ -19,15 +19,21 @@ DB_USER="$MONGO_INITDB_ROOT_USERNAME"  # Database username
 DB_PASS="$MONGO_INITDB_ROOT_PASSWORD"	 # Database password
 
 # Backup directory and filename
-BACKUP_DIR="/data/db/backup" 
+BACKUP_DIR="/data/db/backup"
 BACKUP_FILENAME="cytomine_mongo_backup_$(date "+%a")" # Use day of the week for unique backup directories
+CURR_MONTH_BACKUP_FILENAME="cytomine_mongo_backup_current_month"
+PREV_MONTH_BACKUP_FILENAME="cytomine_mongo_backup_previous_month"
+
+mkdir -p $BACKUP_DIR
 
 # Full path to the backup file
 BACKUP_TARGET_PATH="$BACKUP_DIR/$BACKUP_FILENAME.tar.gz"
-mkdir -p $BACKUP_DIR
+CURR_MONTH_BACKUP_TARGET_PATH="$BACKUP_DIR/$CURR_MONTH_BACKUP_FILENAME.tar.gz"
+PREV_MONTH_BACKUP_TARGET_PATH="$BACKUP_DIR/$PREV_MONTH_BACKUP_FILENAME.tar.gz"
 
 BACKUP_TMP_DIR="/tmp/backups"
 echo "$(date)  Create and clean temporary directory ${BACKUP_TMP_DIR}"
+
 mkdir -p $BACKUP_TMP_DIR
 rm -rf "${BACKUP_TMP_DIR:?}/*"
 
@@ -56,6 +62,16 @@ fi
 
 # Clean tmp files
 rm $BACKUP_TMP_DIR -rf
+
+# If first of the month, update the current month backup with the daily backup. If not first of the month but current month backup does not exist (new instances), update the current month backup with the daily backup
+if [ $(date "+%d") -eq 01 ] || [ ! -f "$CURR_MONTH_BACKUP_TARGET_PATH" ]; then
+  # If the current month backup exists, move it to the previous month
+  if [ -f "$CURR_MONTH_BACKUP_TARGET_PATH" ]; then
+    cp "$CURR_MONTH_BACKUP_TARGET_PATH" "$PREV_MONTH_BACKUP_TARGET_PATH"
+  fi
+  # Update the current month backup with the daily backup
+  cp "$BACKUP_TARGET_PATH" "$CURR_MONTH_BACKUP_TARGET_PATH"
+fi
 
 echo "$(date) Backup completed successfully: $BACKUP_DIR"
 exit 0
