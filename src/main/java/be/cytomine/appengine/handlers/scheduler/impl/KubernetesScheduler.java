2af6b20c10ed0523698c73ffefd62f159e460fa1
Ba Thien Le <ba.le@uliege.be>
2024-12-02 11:13:39 +0100

    refactor: task execution in a single pod (#13)

--- src/main/java/be/cytomine/appengine/handlers/scheduler/impl/KubernetesScheduler.java
+++ src/main/java/be/cytomine/appengine/handlers/scheduler/impl/KubernetesScheduler.java
@@ -4,31 +4,28 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.Map;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.event.EventListener;
-import org.springframework.core.env.Environment;
 
-import be.cytomine.appengine.dto.handlers.scheduler.Schedule;
-import be.cytomine.appengine.exceptions.SchedulingException;
-import be.cytomine.appengine.handlers.SchedulerHandler;
-import be.cytomine.appengine.handlers.scheduler.impl.utils.PodEvent;
-import be.cytomine.appengine.handlers.scheduler.impl.utils.PodWatcher;
-import be.cytomine.appengine.models.task.Run;
-import be.cytomine.appengine.models.task.Task;
 import io.fabric8.kubernetes.api.model.HostPathVolumeSourceBuilder;
-import io.fabric8.kubernetes.api.model.Pod;
 import io.fabric8.kubernetes.api.model.PodBuilder;
 import io.fabric8.kubernetes.api.model.VolumeBuilder;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClientException;
 import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+
+import be.cytomine.appengine.dto.handlers.scheduler.Schedule;
+import be.cytomine.appengine.exceptions.SchedulingException;
+import be.cytomine.appengine.handlers.SchedulerHandler;
+import be.cytomine.appengine.handlers.scheduler.impl.utils.PodInformer;
+import be.cytomine.appengine.models.task.Run;
+import be.cytomine.appengine.models.task.Task;
+
+@Slf4j
 public class KubernetesScheduler implements SchedulerHandler {
-
-    private static final Logger logger = LoggerFactory.getLogger(KubernetesScheduler.class);
 
     @Autowired
     private Environment environment;
@@ -37,7 +34,7 @@ public class KubernetesScheduler implements SchedulerHandler {
     private KubernetesClient kubernetesClient;
 
     @Autowired
-    private PodWatcher podWatcher;
+    private PodInformer podInformer;
 
     @Value("${app-engine.api_prefix}")
     private String apiPrefix;
@@ -86,94 +83,9 @@ public class KubernetesScheduler implements SchedulerHandler {
         this.baseOutputPath = "/tmp/app-engine/task-run-outputs-";
     }
 
-    @EventListener
-    private void schedulePostTask(PodEvent event) throws SchedulingException {
-        Map<String, String> labels = event.getLabels();
-        String runId = labels.get("runId");
-        String podName = "outputs-sending-" + runId;
-
-        // Avoid creating the post task on every watch event
-        Pod pod = kubernetesClient
-                .pods()
-                .inNamespace("default")
-                .withName(podName)
-                .get();
-        if (pod != null) {
-            return;
-        }
-
-        logger.info("Schedule: create post task pod...");
-
-        String url = baseUrl + runId;
-        String outputFolder = labels.get("outputFolder");
-
-        // Post container commands
-        String installDeps = "apk update && apk --no-cache add curl zip";
-        String sendOutputs = "curl -X POST -F 'outputs=@outputs.zip' " + url + "/outputs.zip";
-        String zipOutputs = "zip -rj outputs.zip " + outputFolder;
-        String and = " && ";
-
-        Map<String, String> podLabels = new HashMap<>() {
-            {
-                put("postTask", "true");
-                put("runId", runId);
-            }
-        };
-        PodBuilder podBuilder = new PodBuilder()
-                .withNewMetadata()
-                .withName(podName)
-                .withLabels(podLabels)
-                .endMetadata();
-
-        // Defining the pod image to run
-        podBuilder
-                .withNewSpec()
-
-                // Add post-container for sending outputs
-                .addNewContainer()
-                .withName(podName)
-                .withImage("alpine:latest")
-                .withImagePullPolicy("IfNotPresent")
-                .withCommand("/bin/sh", "-c", installDeps + and + zipOutputs + and + sendOutputs)
-
-                // Mount volume for outputs
-                .addNewVolumeMount()
-                .withName("outputs")
-                .withMountPath(outputFolder)
-                .endVolumeMount()
-
-                .endContainer()
-
-                // Mount volume from the scheduler file system
-                .addToVolumes(new VolumeBuilder()
-                        .withName("outputs")
-                        .withHostPath(new HostPathVolumeSourceBuilder()
-                                .withPath(baseOutputPath + runId)
-                                .build())
-                        .build())
-
-                // Never restart the pod
-                .withRestartPolicy("Never")
-
-                .endSpec();
-
-        logger.info("Schedule: Post task Pod scheduled to run on the cluster");
-        try {
-            kubernetesClient
-                    .pods()
-                    .inNamespace("default")
-                    .resource(podBuilder.build())
-                    .create();
-        } catch (KubernetesClientException e) {
-            e.printStackTrace();
-            throw new SchedulingException("Post task failed to be scheduled on the cluster");
-        }
-        logger.info("Schedule: Post task Pod queued for execution on the cluster");
-    }
-
     @Override
     public Schedule schedule(Schedule schedule) throws SchedulingException {
-        logger.info("Schedule: get Task parameters");
+        log.info("Schedule: get Task parameters");
 
         Run run = schedule.getRun();
         String runId = run.getId().toString();
@@ -187,16 +99,16 @@ public class KubernetesScheduler implements SchedulerHandler {
         String installDeps = "apk update && apk --no-cache add curl zip";
         String fetchInputs = "curl -L -o inputs.zip " + url + "/inputs.zip";
         String unzipInputs = "unzip -o inputs.zip -d " + task.getInputFolder();
+        String sendOutputs = "curl -X POST -F 'outputs=@outputs.zip' " + url + "/outputs.zip";
+        String zipOutputs = "zip -rj outputs.zip " + task.getOutputFolder();
+        String wait = "while [ ! -f /outputs/finished ]; do sleep 2; done; rm /outputs/finished";
         String and = " && ";
 
-        Map<String, String> labels = new HashMap<>() {
-            {
-                put("postTask", "false");
-                put("runId", runId);
-            }
-        };
+        Map<String, String> labels = new HashMap<>() {{
+            put("runId", runId);
+        }};
 
-        logger.info("Schedule: create task pod...");
+        log.info("Schedule: create task pod...");
         PodBuilder podBuilder = new PodBuilder()
                 .withNewMetadata()
                 .withName(podName)
@@ -207,9 +119,9 @@ public class KubernetesScheduler implements SchedulerHandler {
         podBuilder
                 .withNewSpec()
 
-                // Add pre-container for inputs provisioning
+                // Pre-task for inputs provisioning
                 .addNewInitContainer()
-                .withName("inputs-provisioning-" + runId)
+                .withName("inputs-provisioning")
                 .withImage("alpine:latest")
                 .withImagePullPolicy("IfNotPresent")
                 .withCommand("/bin/sh", "-c", installDeps + and + fetchInputs + and + unzipInputs)
@@ -222,17 +134,36 @@ public class KubernetesScheduler implements SchedulerHandler {
 
                 .endInitContainer()
 
-                // Add Task container
+                // Task container
                 .addNewContainer()
-                .withName(podName)
+                .withName("task")
                 .withImage(imageName)
                 .withImagePullPolicy("IfNotPresent")
+                .withTerminationMessagePath("/outputs/finished")
 
                 // Mount volumes for inputs and outputs
                 .addNewVolumeMount()
                 .withName("inputs")
                 .withMountPath(task.getInputFolder())
                 .endVolumeMount()
+
+                .addNewVolumeMount()
+                .withName("outputs")
+                .withMountPath(task.getOutputFolder())
+                .endVolumeMount()
+
+                .endContainer()
+
+                // Post Task for outputs sending
+                .addNewContainer()
+                .withName("outputs-sending")
+                .withImage("alpine:latest")
+                .withImagePullPolicy("IfNotPresent")
+                .withCommand(
+                    "/bin/sh",
+                    "-c",
+                    installDeps + and + wait + and + zipOutputs + and + sendOutputs
+                )
 
                 .addNewVolumeMount()
                 .withName("outputs")
@@ -260,7 +191,7 @@ public class KubernetesScheduler implements SchedulerHandler {
 
                 .endSpec();
 
-        logger.info("Schedule: Task Pod scheduled to run on the cluster");
+        log.info("Schedule: Task Pod scheduled to run on the cluster");
         try {
             kubernetesClient
                     .pods()
@@ -271,14 +202,14 @@ public class KubernetesScheduler implements SchedulerHandler {
             e.printStackTrace();
             throw new SchedulingException("Task Pod failed to be scheduled on the cluster");
         }
-        logger.info("Schedule: Task Pod queued for execution on the cluster");
+        log.info("Schedule: Task Pod queued for execution on the cluster");
 
         return schedule;
     }
 
     @Override
     public void alive() throws SchedulingException {
-        logger.info("Alive: check if the scheduler is up and running");
+        log.info("Alive: check if the scheduler is up and running");
 
         try {
             kubernetesClient
@@ -293,15 +224,12 @@ public class KubernetesScheduler implements SchedulerHandler {
     @Override
     @PostConstruct
     public void monitor() throws SchedulingException {
-        logger.info("Monitor: add watcher to the cluster");
-
-        try {
-            kubernetesClient
-                    .pods()
-                    .inNamespace("default")
-                    .watch(podWatcher);
-        } catch (KubernetesClientException e) {
-            throw new SchedulingException("Failed to add watcher to the cluster");
-        }
+        log.info("Monitor: add informer to the cluster");
+        kubernetesClient
+            .pods()
+            .inNamespace("default")
+            .inform(podInformer)
+            .run();
+        log.info("Monitor: informer added");
     }
 }
---
+++ src/main/java/be/cytomine/appengine/handlers/scheduler/impl/utils/PodInformer.java
@@ -0,0 +1,78 @@
+package be.cytomine.appengine.handlers.scheduler.impl.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+
+import io.fabric8.kubernetes.api.model.Pod;
+import io.fabric8.kubernetes.client.informers.ResourceEventHandler;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.stereotype.Component;
+
+import be.cytomine.appengine.models.task.Run;
+import be.cytomine.appengine.repositories.RunRepository;
+import be.cytomine.appengine.states.TaskRunState;
+
+@Slf4j
+@AllArgsConstructor
+@Component
+public class PodInformer implements ResourceEventHandler<Pod> {
+
+    private static final Map<String, TaskRunState> STATUS = new HashMap<String, TaskRunState>() {{
+        put("Pending", TaskRunState.PENDING);
+        put("Running", TaskRunState.RUNNING);
+        put("Succeeded", TaskRunState.RUNNING);
+        put("Failed", TaskRunState.FAILED);
+        put("Unknown", TaskRunState.FAILED);
+    }};
+
+    private static final Set<TaskRunState> FINAL_STATES = Set.of(TaskRunState.FAILED, TaskRunState.FINISHED);
+
+    private final RunRepository runRepository;
+
+    private Run getRun(Pod pod) {
+        Map<String, String> labels = pod.getMetadata().getLabels();
+
+        String runId = labels.get("runId");
+        Optional<Run> runOptional = runRepository.findById(UUID.fromString(runId));
+        if (runOptional.isEmpty()) {
+            log.error("Pod Watcher: run {} is empty", runId);
+            return null;
+        }
+
+        return runOptional.get();
+    }
+
+    @Override
+    public void onAdd(Pod pod) {
+        Run run = getRun(pod);
+        if (FINAL_STATES.contains(run.getState())) {
+            return;
+        }
+
+        run.setState(TaskRunState.QUEUED);
+        run = runRepository.saveAndFlush(run);
+        log.info("Pod Informer: updated Run {} to {}", run.getId(), run.getState());
+    }
+
+    @Override
+    public void onUpdate(Pod oldPod, Pod newPod) {
+        Run run = getRun(newPod);
+        if (FINAL_STATES.contains(run.getState())) {
+            return;
+        }
+
+        run.setState(STATUS.getOrDefault(newPod.getStatus().getPhase(), TaskRunState.FAILED));
+        run = runRepository.saveAndFlush(run);
+        log.info("Pod Informer: updated Run {} to {}", run.getId(), run.getState());
+    }
+
+    @Override
+    public void onDelete(Pod pod, boolean deletedFinalStateUnknown) {
+        log.info("Pod Informer: Pod deleted");
+    }
+}
