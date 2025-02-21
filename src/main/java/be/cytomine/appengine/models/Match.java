29dc2df97f0d139c552883d5b86f954304551185
sidhamed <siddiqhamed2017@gmail.com>
2025-02-21 15:58:58 +0100

    refactor: added Match relation

---
+++ src/main/java/be/cytomine/appengine/models/Match.java
@@ -0,0 +1,48 @@
+package be.cytomine.appengine.models;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import be.cytomine.appengine.models.task.Parameter;
+
+
+@Entity
+@Table(name = "match")
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class Match extends BaseEntity {
+
+    @Id
+    @Column(name = "id", updatable = false, nullable = false)
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "matching_id")
+    private Parameter matching;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "matched_id")
+    private Parameter matched;
+
+    public Match(Parameter matching, Parameter matched)
+    {
+        this.matching = matching;
+        this.matched = matched;
+    }
+}
