6cd8e1d0dc7bdabe8438409f475f048a4cff7b1a
sidhamed <siddiqhamed2017@gmail.com>
2025-03-10 16:25:03 +0100

    refactor: added array and item to Parameter types

--- src/main/java/be/cytomine/appengine/dto/inputs/task/ParameterType.java
+++ src/main/java/be/cytomine/appengine/dto/inputs/task/ParameterType.java
@@ -1,13 +1,15 @@
 package be.cytomine.appengine.dto.inputs.task;
 
 public enum ParameterType { // add types to this enum
-    BOOLEAN,
-    INTEGER,
-    NUMBER,
-    STRING,
-    ENUMERATION,
-    GEOMETRY,
-    IMAGE,
-    WSI,
-    FILE
+  BOOLEAN,
+  INTEGER,
+  NUMBER,
+  STRING,
+  ENUMERATION,
+  GEOMETRY,
+  IMAGE,
+  WSI,
+  FILE,
+  ARRAY,
+  ARRAY_ITEM
 }
