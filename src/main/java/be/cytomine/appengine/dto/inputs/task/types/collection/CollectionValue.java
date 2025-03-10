0b5f56b20f3fa3d6d6eac8e8cb7ef7aa5c990545
sidhamed <siddiqhamed2017@gmail.com>
2025-03-10 16:31:00 +0100

    refactor: added annotation

--- src/main/java/be/cytomine/appengine/dto/inputs/task/types/collection/CollectionValue.java
+++ src/main/java/be/cytomine/appengine/dto/inputs/task/types/collection/CollectionValue.java
@@ -5,15 +5,19 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import be.cytomine.appengine.dto.inputs.task.TaskRunParameterValue;
 
 
+@EqualsAndHashCode(callSuper = false)
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CollectionValue extends TaskRunParameterValue {
     @JsonIgnoreProperties({"param_name"})
     private List<TaskRunParameterValue> value;
+
+
 }
