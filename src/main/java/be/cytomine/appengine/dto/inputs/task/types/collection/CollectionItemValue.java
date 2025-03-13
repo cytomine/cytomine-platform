278cfb6904be657329438250e3f3a46b4393b237
sidhamed <siddiqhamed2017@gmail.com>
2025-03-13 11:01:23 +0100

    fix: ignore null properties

--- src/main/java/be/cytomine/appengine/dto/inputs/task/types/collection/CollectionItemValue.java
+++ src/main/java/be/cytomine/appengine/dto/inputs/task/types/collection/CollectionItemValue.java
@@ -2,6 +2,7 @@ package be.cytomine.appengine.dto.inputs.task.types.collection;
 
 import be.cytomine.appengine.dto.inputs.task.TaskRunParameterValue;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,11 +11,12 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@JsonIgnoreProperties({ "task_run_id", "type" , "param_name"})
+@JsonIgnoreProperties({"task_run_id", "type", "param_name"})
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CollectionItemValue extends TaskRunParameterValue {
 
-    @JsonProperty("index")
-    protected int index;
-    private Object value;
+  @JsonProperty("index")
+  protected int index;
 
+  private Object value;
 }
