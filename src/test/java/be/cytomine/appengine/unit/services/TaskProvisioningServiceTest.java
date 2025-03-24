de0bc32ec7e5514cdebca364669bbc980f384262
Ba Thien LE <bathien.le.pro@gmail.com>
2025-03-24 15:51:45 +0100

    test: refactor test

--- src/test/java/be/cytomine/appengine/unit/services/TaskProvisioningServiceTest.java
+++ src/test/java/be/cytomine/appengine/unit/services/TaskProvisioningServiceTest.java
@@ -6,13 +6,12 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.context.ApplicationContext;
@@ -23,7 +22,6 @@ import be.cytomine.appengine.handlers.StorageData;
 import be.cytomine.appengine.handlers.StorageHandler;
 import be.cytomine.appengine.models.task.Run;
 import be.cytomine.appengine.repositories.RunRepository;
-import be.cytomine.appengine.repositories.TypePersistenceRepository;
 import be.cytomine.appengine.repositories.integer.IntegerPersistenceRepository;
 import be.cytomine.appengine.services.TaskProvisioningService;
 import be.cytomine.appengine.utils.AppEngineApplicationContext;
@@ -47,20 +45,17 @@ public class TaskProvisioningServiceTest {
     @Mock
     private RunRepository runRepository;
 
-    @Mock
-    private TypePersistenceRepository typePersistenceRepository;
-
     @InjectMocks
     private TaskProvisioningService taskProvisioningService;
 
-    private ApplicationContext applicationContext;
+    private static ApplicationContext applicationContext;
 
-    private IntegerPersistenceRepository integerPersistenceRepository;
+    private static IntegerPersistenceRepository integerPersistenceRepository;
 
-    private Run run;
+    private static Run run;
 
-    @BeforeEach
-    public void setUp() throws Exception {
+    @BeforeAll
+    public static void setUp() {
         run = TaskUtils.createTestRun();
         applicationContext = mock(ApplicationContext.class);
 
@@ -81,15 +76,15 @@ public class TaskProvisioningServiceTest {
         value.put("value", 42);
 
         Mockito.when(runRepository.findById(run.getId())).thenReturn(Optional.of(run));
-        try (MockedStatic<AppEngineApplicationContext> mockedContext = Mockito.mockStatic(AppEngineApplicationContext.class)) {
-            mockedContext.when(() -> AppEngineApplicationContext.getBean(IntegerPersistenceRepository.class)).thenReturn(integerPersistenceRepository);
-        }
 
-        JsonNode response = taskProvisioningService.provisionRunParameter(run.getId().toString(), name, value);
+        JsonNode result = taskProvisioningService.provisionRunParameter(run.getId().toString(), name, value);
 
-        Assertions.assertNotNull(response);
-        Mockito.verify(runRepository).findById(run.getId());
-        Mockito.verify(storageHandler).saveStorageData(any(Storage.class), any(StorageData.class));
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(value.get("param_name"), result.get("param_name"));
+        Assertions.assertEquals(value.get("value"), result.get("value"));
+        Assertions.assertEquals(run.getId().toString(), result.get("task_run_id").asText());
+        Mockito.verify(runRepository, Mockito.times(1)).findById(run.getId());
+        Mockito.verify(storageHandler, Mockito.times(1)).saveStorageData(any(Storage.class), any(StorageData.class));
     }
 
     // @DisplayName("Successfully provision a run parameter with binary data")
