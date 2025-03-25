f71dc1002ab88ef159442628b8ea9526115c8099
Ba Thien LE <bathien.le.pro@gmail.com>
2025-03-25 13:23:55 +0100

    refactor: assertions and mock imports

--- src/test/java/be/cytomine/appengine/unit/services/TaskValidationServiceTest.java
+++ src/test/java/be/cytomine/appengine/unit/services/TaskValidationServiceTest.java
@@ -4,14 +4,12 @@ import java.nio.file.Files;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import be.cytomine.appengine.dto.inputs.task.UploadTaskArchive;
@@ -20,6 +18,13 @@ import be.cytomine.appengine.models.task.Task;
 import be.cytomine.appengine.repositories.TaskRepository;
 import be.cytomine.appengine.services.TaskValidationService;
 import be.cytomine.appengine.utils.TaskUtils;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class TaskValidationServiceTest {
@@ -45,10 +50,10 @@ public class TaskValidationServiceTest {
     public void checkIsNotDuplicateShouldReturnVoid() throws Exception {
         String namespace = archive.getDescriptorFileAsJson().get("namespace").textValue();
         String version = archive.getDescriptorFileAsJson().get("version").textValue();
-        Mockito.when(taskRepository.findByNamespaceAndVersion(namespace, version)).thenReturn(null);
+        when(taskRepository.findByNamespaceAndVersion(namespace, version)).thenReturn(null);
 
-        Assertions.assertDoesNotThrow(() -> taskValidationService.checkIsNotDuplicate(archive));
-        Mockito.verify(taskRepository, Mockito.times(1)).findByNamespaceAndVersion(namespace, version);
+        assertDoesNotThrow(() -> taskValidationService.checkIsNotDuplicate(archive));
+        verify(taskRepository, times(1)).findByNamespaceAndVersion(namespace, version);
     }
 
     @DisplayName("Successfully throw 'ValidationException' when duplicates")
@@ -56,20 +61,20 @@ public class TaskValidationServiceTest {
     public void checkIsNotDuplicateShouldThrowValidationException() {
         String namespace = archive.getDescriptorFileAsJson().get("namespace").textValue();
         String version = archive.getDescriptorFileAsJson().get("version").textValue();
-        Mockito.when(taskRepository.findByNamespaceAndVersion(namespace, version)).thenReturn(task);
+        when(taskRepository.findByNamespaceAndVersion(namespace, version)).thenReturn(task);
 
-        ValidationException exception = Assertions.assertThrows(
+        ValidationException exception = assertThrows(
             ValidationException.class,
             () -> taskValidationService.checkIsNotDuplicate(archive)
         );
-        Assertions.assertEquals("Task already exists.", exception.getMessage());
-        Mockito.verify(taskRepository, Mockito.times(1)).findByNamespaceAndVersion(namespace, version);
+        assertEquals("Task already exists.", exception.getMessage());
+        verify(taskRepository, times(1)).findByNamespaceAndVersion(namespace, version);
     }
 
     @DisplayName("Successfully return void when image is valid")
     @Test
     public void validateImageShouldReturnVoid() throws Exception {
-        Assertions.assertDoesNotThrow(() -> taskValidationService.validateImage(archive));
+        assertDoesNotThrow(() -> taskValidationService.validateImage(archive));
     }
 
     @DisplayName("Fail to validate image and throw 'ValidationException'")
@@ -78,17 +83,17 @@ public class TaskValidationServiceTest {
         UploadTaskArchive missing = TaskUtils.createTestUploadTaskArchive();
         missing.setDockerImage(Files.createTempFile("docker-image", ".tar").toFile());
 
-        ValidationException exception = Assertions.assertThrows(
+        ValidationException exception = assertThrows(
             ValidationException.class,
             () -> taskValidationService.validateImage(missing)
         );
-        Assertions.assertEquals("image is not invalid manifest is missing", exception.getMessage());
+        assertEquals("image is not invalid manifest is missing", exception.getMessage());
     }
 
     @DisplayName("Successfully validate the descriptor")
     @Test
     public void validateDescriptorFileShouldReturnVoid() throws Exception {
-        Assertions.assertDoesNotThrow(() -> taskValidationService.validateDescriptorFile(archive));
+        assertDoesNotThrow(() -> taskValidationService.validateDescriptorFile(archive));
     }
 
     @DisplayName("Fail to validate the descriptor throw 'ValidationException'")
@@ -101,10 +106,10 @@ public class TaskValidationServiceTest {
         node.put("name", "a");
         invalid.setDescriptorFileAsJson(descriptor);
 
-        ValidationException exception = Assertions.assertThrows(
+        ValidationException exception = assertThrows(
             ValidationException.class,
             () -> taskValidationService.validateDescriptorFile(invalid)
         );
-        Assertions.assertEquals("schema validation failed for descriptor.yml", exception.getMessage());
+        assertEquals("schema validation failed for descriptor.yml", exception.getMessage());
     }
 }
