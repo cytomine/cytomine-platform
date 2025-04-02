378cf83da1f573edee6fb83b102a040fb74b493c
sidhamed <siddiqhamed2017@gmail.com>
2025-04-02 13:24:53 +0200

    fix: fix style

--- src/main/java/be/cytomine/appengine/models/task/collection/CollectionPersistence.java
+++ src/main/java/be/cytomine/appengine/models/task/collection/CollectionPersistence.java
@@ -2,13 +2,11 @@ package be.cytomine.appengine.models.task.collection;
 
 import java.util.List;
 
-import be.cytomine.appengine.models.task.TypePersistence;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -17,6 +15,8 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
+
+import be.cytomine.appengine.models.task.TypePersistence;
 
 @Entity
 @Table(name = "collection_type_persistence")
@@ -32,7 +32,7 @@ public class CollectionPersistence extends TypePersistence {
     @Column(columnDefinition = "json")
     private String compactValue;
 
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JoinColumn(name = "collection_id")
     private List<TypePersistence> items;
 }
