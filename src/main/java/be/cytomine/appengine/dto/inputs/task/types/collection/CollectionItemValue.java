9c10e35b2d9160ffff164035f52785e9c70df09a
sidhamed <siddiqhamed2017@gmail.com>
2025-04-03 17:09:19 +0200

    fix: style

--- src/main/java/be/cytomine/appengine/dto/inputs/task/types/collection/CollectionItemValue.java
+++ src/main/java/be/cytomine/appengine/dto/inputs/task/types/collection/CollectionItemValue.java
@@ -1,12 +1,13 @@
 package be.cytomine.appengine.dto.inputs.task.types.collection;
 
-import be.cytomine.appengine.dto.inputs.task.TaskRunParameterValue;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import be.cytomine.appengine.dto.inputs.task.TaskRunParameterValue;
 
 @Data
 @AllArgsConstructor
@@ -15,8 +16,7 @@ import lombok.NoArgsConstructor;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class CollectionItemValue extends TaskRunParameterValue {
 
-  @JsonProperty("index")
-  protected int index;
-
-  private Object value;
+    @JsonProperty("index")
+    protected int index;
+    private Object value;
 }
