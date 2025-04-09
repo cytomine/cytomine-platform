41c994565b66f41644fca3d691516836b5fed411
sidhamed <siddiqhamed2017@gmail.com>
2025-04-09 13:03:19 +0200

    fix: remove unused import

--- src/main/java/be/cytomine/appengine/handlers/scheduler/impl/KubernetesScheduler.java
+++ src/main/java/be/cytomine/appengine/handlers/scheduler/impl/KubernetesScheduler.java
@@ -20,7 +20,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Component;
 
 import be.cytomine.appengine.dto.handlers.scheduler.Schedule;
 import be.cytomine.appengine.exceptions.SchedulingException;
