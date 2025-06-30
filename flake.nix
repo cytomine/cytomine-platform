7711493753e4244a1b9a0f378efee817c86a086e
Luca Di Carlo <luca.dicarlo@uliege.be>
2025-06-30 11:13:26 +0200

    chore: add nix

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs;[
+            openssl
+            fluxcd
+            kubectl
+            postgresql
+            kustomize
+            kubernetes-helm
+          ];
+          shellHook = ''
+            export KUBECONFIG=./.kube/config
+          '';
+        };
+      });
+}
