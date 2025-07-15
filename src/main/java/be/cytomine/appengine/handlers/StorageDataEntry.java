c99b39a5c83d9d62dfbc583c5f30ca23fb986aa8
sidhamed <siddiqhamed2017@gmail.com>
2025-07-15 16:14:03 +0200

    feat: new constructor

--- src/main/java/be/cytomine/appengine/handlers/StorageDataEntry.java
+++ src/main/java/be/cytomine/appengine/handlers/StorageDataEntry.java
@@ -1,21 +1,11 @@
 package be.cytomine.appengine.handlers;
 
 import java.io.File;
-import java.util.UUID;
 
-import be.cytomine.appengine.exceptions.FileStorageException;
-import be.cytomine.appengine.models.task.Checksum;
-import be.cytomine.appengine.models.task.Run;
-import be.cytomine.appengine.repositories.ChecksumRepository;
-import be.cytomine.appengine.repositories.integer.IntegerPersistenceRepository;
-import be.cytomine.appengine.utils.AppEngineApplicationContext;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Component;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class StorageDataEntry
 {
@@ -27,6 +17,7 @@ public class StorageDataEntry
     private String storageId;
 
     private StorageDataType storageDataType;
+
 
     public StorageDataEntry(File data)
     {
@@ -64,21 +55,10 @@ public class StorageDataEntry
         this.storageId = storageId;
     }
 
-    public long getChecksumCRC32(UUID uuid) throws FileStorageException
-    {
-        ChecksumRepository checksumRepository =
-            AppEngineApplicationContext.getBean(ChecksumRepository.class);
-        String reference = uuid.toString() + "-" + name;
-        Checksum crc32 = checksumRepository.findByReference(reference);
-        return crc32.getChecksumCRC32();
-    }
-
-    public void setChecksumCRC32(UUID uuid, long checksumCRC32)
-    {
-        ChecksumRepository checksumRepository = AppEngineApplicationContext.getBean(ChecksumRepository.class);
-        String reference = uuid.toString() + "-" + name;
-        Checksum crc32 = new Checksum(reference, checksumCRC32);
-
-        checksumRepository.save(crc32);
+    public StorageDataEntry(File data, String parameterName, String storageId, StorageDataType storageDataType) {
+        this.data = data;
+        this.name = parameterName;
+        this.storageId = storageId;
+        this.storageDataType = storageDataType;
     }
 }
